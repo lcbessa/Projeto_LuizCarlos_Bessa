a883499af88dd0bfb01a97f63be52988504bd3d1
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2021-08-29 21:52:37 -0300

    Modificação na classe A

--- Classe_A.cpp
+++ Classe_A.cpp
@@ -13,6 +13,7 @@ public:
     int SetA2(float A2);
     void MA1();
     void MA2();
+    void MA3();
 };
 
 Classe_A::Classe_A() {
@@ -26,7 +27,7 @@ void Classe_A::GetA2() {
     A2 = A2;
 }
 int Classe_A::SetA1(int A1) {
-    return A2;
+    return A1;
 }
 int Classe_A::SetA2(float A2) {
     return A2;
@@ -37,10 +38,14 @@ void Classe_A::MA1() {
 void Classe_A::MA2() {
     cout << "MA2()"<< endl;
 }
+void Classe_A::MA3() {
+    cout << "”Alteração a classe A partir do clone”"<< endl;
+}
 
 int main () {
     Classe_A A;
     A.MA1();
     A.MA2();
+    A.MA3();
     return 0;
 }
