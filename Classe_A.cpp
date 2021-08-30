df74cbd90b1af799e698103c86771a558f0c8058
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2021-08-29 21:15:33 -0300

    initial commit

---
+++ Classe_A.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+class Classe_A {
+private:
+    int A1;
+    float A2;
+public:
+    Classe_A();
+    void GetA1();
+    void GetA2();
+    int SetA1(int A1);
+    int SetA2(float A2);
+    void MA1();
+    void MA2();
+};
+
+Classe_A::Classe_A() {
+    A1 = 0;
+    A2 = 0;
+}
+void Classe_A::GetA1() {
+    A1 = A1;
+}
+void Classe_A::GetA2() {
+    A2 = A2;
+}
+int Classe_A::SetA1(int A1) {
+    return A2;
+}
+int Classe_A::SetA2(float A2) {
+    return A2;
+}
+void Classe_A::MA1() {
+    cout << "MA1()" << endl;
+}
+void Classe_A::MA2() {
+    cout << "MA2()"<< endl;
+}
+
+int main () {
+    Classe_A A;
+    A.MA1();
+    A.MA2();
+    return 0;
+}
---
+++ Classe_B.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+class Classe_B {
+private:
+    int B1;
+    float B2;
+public:
+    Classe_B();
+    void GetB1();
+    void GetB2();
+    int SetB1(int B1);
+    int SetB2(float B2);
+    void MB1();
+    void MB2();
+};
+
+Classe_B::Classe_B() {
+    B1 = 0;
+    B2 = 0;
+}
+void Classe_B::GetB1() {
+    B1 = B1;
+}
+void Classe_B::GetB2() {
+    B2 = B2;
+}
+int Classe_B::SetB1(int B1) {
+    return B2;
+}
+int Classe_B::SetB2(float B2) {
+    return B2;
+}
+void Classe_B::MB1() {
+    cout << "MB1()" << endl;
+}
+void Classe_B::MB2() {
+    cout << "MB2()"<< endl;
+}
+
+int main () {
+    Classe_B B;
+    B.MB1();
+    B.MB2();
+    return 0;
+}
